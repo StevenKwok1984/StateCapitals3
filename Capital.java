ed11666bab302783c86ba007b1edea36ae304ad6
Steven Kwok <derekkwok2020@gmail.com>
2022-06-21 11:01:18 +0100

    Not Completed

--- Capital.java
+++ Capital.java
@@ -3,4 +3,37 @@ public class Capital {
     int population;
     int squareMileage;
 
+    public Capital(){
+
+    }
+
+    public Capital(String name, int population, int squareMileage){
+        this.name = name;
+        this.population = population;
+        this.squareMileage = squareMileage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getSquareMileage() {
+        return squareMileage;
+    }
+
+    public void setSquareMileage(int squareMileage) {
+        this.squareMileage = squareMileage;
+    }
 }
--- StateCapitalsApp.java
+++ StateCapitalsApp.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class StateCapitalsApp {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Capital> stateCapital = new HashMap<>();
+
+        Scanner sc = new Scanner(
+                new BufferedReader(new FileReader("MoreStateCapitals.txt")));
+
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            String[] keyValuePair = str.split("::");
+            stateCapital.put(keyValuePair[0], keyValuePair[1]);
+        }
     }
 }
